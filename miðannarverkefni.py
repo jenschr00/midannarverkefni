f6926142765629a7099db57023466e5b5435c162
jenschr00 <jensas278@nemi.tskoli.is>
2018-03-05 11:58:08 +0000

    Update miðannarverkefni.py

--- miðannarverkefni.py
+++ miðannarverkefni.py
@@ -1,9 +1,28 @@
 from bottle import *
 import urllib.request, json
 import os
+with urllib.request.urlopen("http://apis.is/petrol") as url:
+    data = json.loads(url.read().decode())
+
+@route('/Myndir/<filename>')
+def server_static(filename):
+    return static_file(filename, root="./Myndir")
 
 @route('/')
 def index():
-    return "Virkar"
+    return template("mið.tpl",data=data)
+
+@route('/allt/<name>')
+def allt(name):
+    return template('mið2.tpl',data=data,name=name)
+    
+@route('/allt2/<name>')
+def allt2(name):
+    return template('mið3.tpl',data=data, name=name)
+
+@error(404)
+def villa(error):
+    return("vefsíða ekki til")
+
 
 run(host='0.0.0.0', port=os.environ.get('PORT'))
