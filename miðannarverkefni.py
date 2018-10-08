99701b33e31cebb31cc1f9c541d0d8c5b34e7166
jenschr00 <jensas278@nemi.tskoli.is>
2018-10-08 11:29:24 +0000

    Add files via upload

--- miðannarverkefni.py
+++ miðannarverkefni.py
@@ -1,9 +1,7 @@
 from bottle import *
 import urllib.request, json
-import os
 with urllib.request.urlopen("http://apis.is/petrol") as url:
     data = json.loads(url.read().decode())
-
 @route('/Myndir/<filename>')
 def server_static(filename):
     return static_file(filename, root="./Myndir")
@@ -15,14 +13,12 @@ def index():
 @route('/allt/<name>')
 def allt(name):
     return template('mið2.tpl',data=data,name=name)
-    
-@route('/allt2/<name>')
-def allt2(name):
-    return template('mið3.tpl',data=data, name=name)
+@route('/allt2/<company>/<name>')
+def allt2(company,name):
+    return template('mið3.tpl',data=data,company=company,name=name)
 
 @error(404)
 def villa(error):
     return("vefsíða ekki til")
 
-
-run(host='0.0.0.0', port=os.environ.get('PORT'))
+run(host='localhost', port=8080)
